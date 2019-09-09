adebc8776e06f7d9a6a9f6fda3d9100a937cd306
Messias Junior <messiaslima.03@gmail.com>
2019-09-09 15:07:37 +0100

    feat: adding fields

--- src/model/app.js
+++ src/model/app.js
@@ -2,13 +2,17 @@ const mongoose = require('mongoose')
 const Category = require('./category')
 
 const appSchema = mongoose.Schema({
+    identifier: String,
     name: String,
-    category: [String],
+    category: [{ type: Schema.Types.ObjectId, ref: 'Category' }],
     logo: String,
     screens: [String],
     description: String,
     store: String,
-    installScript: String
+    installScript: String,
+    creationDate: Date,
+    lastUpdate: Date,
+    version: String
 })
 
 const App = mongoose.model('App', appSchema);
