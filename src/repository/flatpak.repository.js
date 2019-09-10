e3f268568301ebc5ddbc34b43b93c43ff437f2bc
Messias Junior <messiaslima.03@gmail.com>
2019-09-10 16:40:28 +0100

    WIP: getting app details

--- src/repository/flatpak.repository.js
+++ src/repository/flatpak.repository.js
@@ -1,10 +1,24 @@
 const axios = require('axios')
 const { from } = require('rxjs')
-const flatpackBaseUrl = "https://flathub.org/api/v1/apps/"
+const { map } = require('rxjs/operators')
+const flatpakBaseUrl = "https://flathub.org/api/v1/apps/"
 
 function getApps() {
-    const request = axios.get(flatpackBaseUrl)
+    const request = axios.get(flatpakBaseUrl)
+    return parseAxiosRequest(request)
+}
+
+function getAppDetails(flatpakAppId) {
+    const request = axios.get(flatpakBaseUrl + flatpakAppId)
+    return parseAxiosRequest(request)
+}
+
+function parseAxiosRequest(request) {
     return from(request)
+        .pipe(
+            map(response => response.data)
+        )
 }
 
 exports.getApps = getApps
+exports.getAppDetails = getAppDetails
