b53fbf10d72cdaa926c2ede56bd075e06a28b394
Messias Junior <messiaslima.03@gmail.com>
2019-09-25 23:13:36 +0100

    feat: download url

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,13 @@
 const { Router } = require('express')
 const appRouter = require('./app.router')
 const categoryRouter = require('./category.router')
+const downloadRouter = require('./download.router')
 
 const router = Router()
 
 router.use('/app', appRouter)
 router.use('/category', categoryRouter)
+router.use('/download', downloadRouter)
 router.get('',(request, response)=>{
     response.redirect('https://app-outlet.github.io')
 })
