3a1f8326247d1e7ac6f8fef5c3ec9ca652973953
Fagner Candido <fagner.candido@globalweb.com.br>
2019-09-29 00:27:44 +0100

    added cache

--- src/index.js
+++ src/index.js
@@ -20,6 +20,9 @@ const clientRedis = redis.createClient(constants.redisUrl)
 clientRedis.on('connect', () => {
     logger.i(`Server connect with redis`)
 });
+
 clientRedis.on('error', err => {
     logger.i(`Server redis with error: ${err}`)
 });
+
+module.exports.clientRedis  = clientRedis
--- src/router/category.router.js
+++ src/router/category.router.js
@@ -1,15 +1,25 @@
 const { Router } = require('express')
 const categoryService = require('../service/category.service')
 const HttpStatus = require('http-status-codes')
+const redis = require('../index')
 
 const router = new Router()
 
 router.get('', (request, response) => {
-    categoryService.findAll().subscribe(apps => {
-        response.send(apps)
-    }, error => {
-        response.status(HttpStatus.BAD_REQUEST)
-        response.send(error)
+    const category = 'category'
+    redis.clientRedis.get(category, function(err, categories) {
+        if (categories) {
+            console.log('category by cache')
+            return response.send(categories)
+        }
+        categoryService.findAll().subscribe(apps => {
+            console.log('category by server')
+            redis.clientRedis.setex(category, 3600, JSON.stringify(apps))
+            response.send(apps)
+        }, error => {
+            response.status(HttpStatus.BAD_REQUEST)
+            response.send(error)
+        })
     })
 })
 
