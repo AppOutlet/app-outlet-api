fb2f6bf9aaddcaeadd9b67c1f6ec1f2759802461
Messias Junior <messiaslima.03@gmail.com>
2019-10-18 13:08:47 +0100

    removing cache dependencies

--- src/router/category.router.js
+++ src/router/category.router.js
@@ -1,21 +1,16 @@
 const { Router } = require('express')
 const categoryService = require('../service/category.service')
-const clientRedis = require('../config/cache-connection')
 const HttpStatus = require('http-status-codes')
-const { map } = require('rxjs/operators')
 const router = new Router()
 
 router.get('', (request, response) => {
-    categoryService.findAll()
-        /*.pipe(
-            map(apps => clientRedis.setex(request.originalUrl, 3600, JSON.stringify(apps)))
-        )*/.subscribe(apps => {
+    categoryService.findAll().subscribe(apps => {
             response.send(apps)
-            }, 
-            error => {
-                console.log('Deu ruim.')
-                response.status(HttpStatus.BAD_REQUEST)
-                response.send(error)
+        },
+        error => {
+            console.log('Deu ruim.')
+            response.status(HttpStatus.BAD_REQUEST)
+            response.send(error)
         })
 })
 
