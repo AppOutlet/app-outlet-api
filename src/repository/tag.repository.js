a90006d1d34be93c03f41440a6f9c497468b46b7
Messias Junior <messiaslima.03@gmail.com>
2019-10-25 17:18:10 +0100

    feat: tag repository

---
+++ src/repository/tag.repository.js
@@ -0,0 +1,24 @@
+const Tag = require('../model/tag')
+
+exports.save = function(tag) {
+
+    const promise = new Promise((resolve, reject) => {
+        Tag.findOneAndUpdate({ _id: tag }, { _id: tag }, { upsert: true }, (err, document) => {
+            if (err) {
+                reject(err)
+            } else {
+                resolve(document)
+            }
+        })
+    })
+
+    return from(promise)
+}
+
+exports.findAll = function() {
+
+    const promise = Tag.find()
+        .sort({ _id: 1 })
+
+    return from(promise)
+}
---
+++ src/router/tag/tag.router.js
@@ -0,0 +1,11 @@
+const { Router } = require('express')
+const tagService = require('../../service/tag/tag.service')
+const HttpStatus = require('http-status-codes')
+
+const router = new Router()
+
+router.get('', (request, response) => {
+
+})
+
+module.exports = router
