dd4efe30777f1f1b5ccf7f4cc89ec4b95d459b16
Messias Junior <messiaslima.03@gmail.com>
2019-10-25 17:20:30 +0100

    feat: tag service

--- src/service/tag/tag.service.js
+++ src/service/tag/tag.service.js
@@ -1,1 +1,9 @@
-exports.
+const tagRepository = require('../../repository/tag.repository')
+
+exports.findAll = function() {
+    return tagRepository.findAll()
+}
+
+exports.save = function(tag) {
+    return tagRepository.save(tag)
+}
