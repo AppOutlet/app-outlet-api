eb0420743e3343eb9de05417565e21c4caaac3a0
Messias Junior <messiaslima.03@gmail.com>
2019-10-30 21:18:30 +0000

    feat: search tags

--- src/router/tag/tag.router.js
+++ src/router/tag/tag.router.js
@@ -5,7 +5,12 @@ const HttpStatus = require('http-status-codes')
 const router = new Router()
 
 router.get('', (request, response) => {
-
+    tagService.findAll().subscribe(tags => {
+        response.send(tags)
+    }, err => {
+        response.status(HttpStatus.BAD_REQUEST)
+        response.send(err)
+    })
 })
 
 module.exports = router
