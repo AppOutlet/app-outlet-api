70a55735527949cd5dbbee78bd8071ffcd6f39bc
Messias Junior <messiaslima.03@gmail.com>
2020-07-20 18:57:40 +0100

    chore: delete old application

--- src/model/category.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose')
-
-const categorySchema = mongoose.Schema({
-    _id: String
-})
-const Category = mongoose.model('Category', categorySchema);
-
-module.exports = Category
