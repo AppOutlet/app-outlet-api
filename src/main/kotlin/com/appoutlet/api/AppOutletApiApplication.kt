80dc32344554ed5376c992468651557c133d6624
Messias Junior <messiaslima.03@gmail.com>
2020-07-20 19:06:45 +0100

    chore: application bootstrap

---
+++ src/main/kotlin/com/appoutlet/api/AppOutletApiApplication.kt
@@ -0,0 +1,11 @@
+package com.appoutlet.api
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AppOutletApiApplication
+
+fun main(args: Array<String>) {
+    runApplication<AppOutletApiApplication>(*args)
+}
---
+++ src/test/kotlin/com/appoutlet/api/AppOutletApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.appoutlet.api
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AppOutletApiApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
