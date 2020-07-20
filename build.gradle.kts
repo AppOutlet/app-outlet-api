eb74ee094be232da834e77fba525690e03fe82a5
Messias Junior <messiaslima.03@gmail.com>
2020-07-20 19:13:20 +0100

    chore: install ktlint

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     id("org.springframework.boot") version "2.3.1.RELEASE"
     id("io.spring.dependency-management") version "1.0.9.RELEASE"
+    id("org.jmailen.kotlinter") version "2.4.1"
     kotlin("jvm") version "1.3.72"
     kotlin("plugin.spring") version "1.3.72"
 }
