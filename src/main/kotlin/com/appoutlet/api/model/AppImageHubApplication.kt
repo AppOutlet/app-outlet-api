6b374672c59eea63648d585870bbe67762e591f4
Messias Junior <messiaslima.03@gmail.com>
2020-07-30 18:41:31 +0100

    feat: convert app image app to app outlet app

--- src/main/kotlin/com/appoutlet/api/model/AppImageHubApplication.kt
+++ src/main/kotlin/com/appoutlet/api/model/AppImageHubApplication.kt
@@ -1,26 +1,29 @@
 package com.appoutlet.api.model
 
+import com.fasterxml.jackson.annotation.JsonValue
+
 data class AppImageHubApplication(
-	val name: String,
-	val description: String,
-	val categories: List<String>,
-	val authors: List<Author>,
-	val license: String,
-	val links: List<Link>,
-	val icons: List<String>,
-	val screenshots: List<String>
+    val name: String,
+    val description: String?,
+    val categories: List<String>?,
+    val authors: List<Author>?,
+    val license: String?,
+    val links: List<Link>?,
+    val icons: List<String>?,
+    val screenshots: List<String>?
 ) {
+	// TODO: separate this data classes on different files and group it on a package for AppImageHub models
 	data class Author(
-		val name: String,
-		val url: String
+	    val name: String,
+	    val url: String?
 	)
 
 	data class Link(
-		val type: LinkType,
-		val url: String
+	    val type: LinkType?,
+	    val url: String?
 	)
 
-	enum class LinkType(val typeName: String) {
-		DOWNLOAD("Download"), GITHUB("GitHub")
+	enum class LinkType(@JsonValue val typeName: String) {
+		DOWNLOAD("Download"), GITHUB("GitHub"), INSTALL("Install")
 	}
 }
--- src/main/kotlin/com/appoutlet/api/service/AppImageHubSynchronizer.kt
+++ src/main/kotlin/com/appoutlet/api/service/AppImageHubSynchronizer.kt
@@ -1,21 +1,86 @@
 package com.appoutlet.api.service
 
+import com.appoutlet.api.model.AppImageHubApplication
+import com.appoutlet.api.model.AppOutletApplication
+import com.appoutlet.api.model.ApplicationPackageType
+import com.appoutlet.api.model.ApplicationStore
 import com.appoutlet.api.repository.AppImageHubRepository
 import org.springframework.stereotype.Service
-import reactor.core.publisher.Flux
 import reactor.core.publisher.Mono
 import reactor.kotlin.core.publisher.toFlux
 import reactor.kotlin.core.publisher.toMono
 
 @Service
 class AppImageHubSynchronizer(
-	private val appImageHubRepository: AppImageHubRepository
-) : Synchronizer{
+    private val appImageHubRepository: AppImageHubRepository
+) : Synchronizer {
 	override fun synchronize(): Mono<Boolean> {
 		return appImageHubRepository.getApps().toFlux()
-			.flatMap { Flux.fromIterable(it.items) }
+			.map(this::convertAppImageApplicationToAppOutletApplication)
+			.map(this::addDownloadUrl)
 			.buffer()
 			.toMono()
 			.map { true }
+	}
+
+	private fun addDownloadUrl(application: AppOutletApplication): AppOutletApplication {
+		return application
+	}
+
+	private fun convertAppImageApplicationToAppOutletApplication(
+	    appImageHubApplication: AppImageHubApplication
+	): AppOutletApplication {
+		return AppOutletApplication(
+			id = getId(appImageHubApplication),
+			name = appImageHubApplication.name,
+			summary = appImageHubApplication.description,
+			description = appImageHubApplication.description,
+			developer = getDeveloper(appImageHubApplication),
+			license = appImageHubApplication.license,
+			homepage = getHomepage(appImageHubApplication),
+			bugtrackerUrl = getBugtrackerUrl(appImageHubApplication),
+			donationUrl = null,
+			icon = getIcon(appImageHubApplication),
+			downloadUrl = null,
+			version = null,
+			lastReleaseDate = null,
+			creationDate = null,
+			tags = appImageHubApplication.categories,
+			screenshots = getScreenshots(appImageHubApplication),
+			store = ApplicationStore.APP_IMAGE_HUB,
+			packageType = ApplicationPackageType.APP_IMAGE
+		)
+	}
+
+	private fun getId(appImageHubApplication: AppImageHubApplication): String {
+		val author = getDeveloper(appImageHubApplication)
+		return "$author.${appImageHubApplication.name}"
+	}
+
+	private fun getDeveloper(appImageHubApplication: AppImageHubApplication): String {
+		return appImageHubApplication.authors?.get(0)?.name ?: "unknown"
+	}
+
+	private fun getHomepage(appImageHubApplication: AppImageHubApplication): String? {
+		val uri = appImageHubApplication.links?.find { it.type == AppImageHubApplication.LinkType.GITHUB }?.url
+		return uri?.let { "https://github.com/$it" }
+	}
+
+	private fun getBugtrackerUrl(appImageHubApplication: AppImageHubApplication): String? {
+		val githubLink = getHomepage(appImageHubApplication)
+		return githubLink?.let { "$githubLink/issues" }
+	}
+
+	private fun getIcon(appImageHubApplication: AppImageHubApplication): String? {
+		val iconUri = appImageHubApplication.icons?.get(0)
+		return iconUri?.let { "https://gitcdn.xyz/repo/AppImage/appimage.github.io/master/database/$it" }
+	}
+
+	private fun getScreenshots(appImageHubApplication: AppImageHubApplication): List<String> {
+		val screenshots = mutableListOf<String>()
+		appImageHubApplication.screenshots?.forEach { imageUri ->
+			screenshots.add("https://appimage.github.io/database/$imageUri")
+		}
+		return screenshots
 	}
 }
