47fc784f9139be56a30e109c2c724e001524547d
Messias Junior <messiaslima.03@gmail.com>
2020-08-06 16:47:54 +0100

    test: AppImageHubSynchronizer

--- src/main/kotlin/com/appoutlet/api/model/appimagehub/AppImageHubAuthor.kt
+++ src/main/kotlin/com/appoutlet/api/model/appimagehub/AppImageHubAuthor.kt
@@ -2,5 +2,5 @@ package com.appoutlet.api.model.appimagehub
 
 data class AppImageHubAuthor(
     val name: String,
-    val url: String?
+    val url: String? = null
 )
---
+++ src/test/kotlin/com/appoutlet/api/service/synchronization/AppImageHubSynchronizerTest.kt
@@ -0,0 +1,63 @@
+package com.appoutlet.api.service.synchronization
+
+import com.appoutlet.api.model.appimagehub.AppImageHubApplication
+import com.appoutlet.api.model.appimagehub.AppImageHubAuthor
+import com.appoutlet.api.model.appimagehub.AppImageHubLink
+import com.appoutlet.api.model.appoutlet.AppOutletApplication
+import com.appoutlet.api.repository.AppOutletApplicationRepository
+import com.appoutlet.api.repository.appimagehub.AppImageHubRepository
+import io.mockk.every
+import io.mockk.mockk
+import org.junit.jupiter.api.Assertions.assertFalse
+import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.BeforeEach
+import reactor.core.publisher.Flux
+
+internal class AppImageHubSynchronizerTest {
+	private lateinit var appImageHubSynchronizer: AppImageHubSynchronizer
+	private val appImageHubRepositoryMock = mockk<AppImageHubRepository>() // TODO: rename all mocks to this pattern
+	private val appOutletApplicationRepositoryMock = mockk<AppOutletApplicationRepository>()
+	private val synchronizationPropertiesMock = mockk<SynchronizationProperties>()
+
+	@BeforeEach
+	fun setup() {
+		every { synchronizationPropertiesMock.appImageHub.enabled } returns false
+		appImageHubSynchronizer = AppImageHubSynchronizer(
+			appImageHubRepositoryMock,
+			appOutletApplicationRepositoryMock,
+			synchronizationPropertiesMock
+		)
+	}
+
+    @Test
+    fun `Should not synchronize if the property is false `() {
+		every { synchronizationPropertiesMock.appImageHub.enabled } returns false
+		assertFalse(appImageHubSynchronizer.synchronize().block() == true)
+    }
+
+	@Test
+	fun `Should synchronize if the property is true `() {
+		val applications = listOf(
+			AppImageHubApplication(
+				authors = listOf(AppImageHubAuthor("Author name")),
+				name = "Application name",
+				description = "Application description",
+				license = "MIT",
+				links = listOf(AppImageHubLink(AppImageHubLink.Type.GITHUB, "app-outlet/app-outlet")),
+				icons = listOf("path/icon.png"),
+				screenshots = listOf("path/screenshot.png"),
+				categories = listOf("Category")
+			)
+		)
+
+		val appOutletApplicationMock = mockk<AppOutletApplication>()
+
+		every { synchronizationPropertiesMock.appImageHub.enabled } returns true
+		every { appImageHubRepositoryMock.getApps() }.returns(Flux.fromIterable(applications))
+		every { appOutletApplicationRepositoryMock.save<AppOutletApplication>(any()) } returns appOutletApplicationMock
+
+		assertTrue(appImageHubSynchronizer.synchronize().block() == true)
+	}
+}
