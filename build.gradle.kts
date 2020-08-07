9cddce3f6196bcdec36279c3d47480d591d68f42
Messias Junior <messiaslima.03@gmail.com>
2020-08-07 10:55:23 +0100

    chore: configure test coverage metrics

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,13 +1,14 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    id("org.springframework.boot") version "2.3.1.RELEASE"
-    id("io.spring.dependency-management") version "1.0.9.RELEASE"
-    id("org.jmailen.kotlinter") version "2.4.1"
-    id("com.star-zero.gradle.githook") version "1.2.0"
-    id("ru.netris.commitlint") version "1.2"
-    kotlin("jvm") version "1.3.72"
-    kotlin("plugin.spring") version "1.3.72"
+	id("org.springframework.boot") version "2.3.1.RELEASE"
+	id("io.spring.dependency-management") version "1.0.9.RELEASE"
+	id("org.jmailen.kotlinter") version "2.4.1"
+	id("com.star-zero.gradle.githook") version "1.2.0"
+	id("ru.netris.commitlint") version "1.2"
+	kotlin("jvm") version "1.3.72"
+	kotlin("plugin.spring") version "1.3.72"
+	jacoco
 }
 
 group = "com.appoutlet"
@@ -15,13 +16,13 @@ version = "2.0.0-ALPHA-0"
 java.sourceCompatibility = JavaVersion.VERSION_11
 
 configurations {
-    compileOnly {
-        extendsFrom(configurations.annotationProcessor.get())
-    }
+	compileOnly {
+		extendsFrom(configurations.annotationProcessor.get())
+	}
 }
 
 repositories {
-    mavenCentral()
+	mavenCentral()
 	maven(url = "https://repo.spring.io/milestone")
 }
 
@@ -34,32 +35,58 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-data-mongodb")
 	implementation("org.springdoc:springdoc-openapi-webflux-ui:1.4.3")
 	implementation("org.springdoc:springdoc-openapi-kotlin:1.4.3")
-    developmentOnly("org.springframework.boot:spring-boot-devtools")
-    annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
+	developmentOnly("org.springframework.boot:spring-boot-devtools")
+	annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
 	testImplementation("com.ninja-squad:springmockk:2.0.2")
-    testImplementation("org.springframework.boot:spring-boot-starter-test") {
-        exclude(group = "org.junit.vintage", module = "junit-vintage-engine")
+	testImplementation("org.springframework.boot:spring-boot-starter-test") {
+		exclude(group = "org.junit.vintage", module = "junit-vintage-engine")
 		exclude(module = "mockito-core")
-    }
+	}
 }
 
 tasks.withType<Test> {
-    useJUnitPlatform()
+	useJUnitPlatform()
 }
 
 tasks.withType<KotlinCompile> {
-    kotlinOptions {
-        freeCompilerArgs = listOf("-Xjsr305=strict")
-        jvmTarget = "11"
-    }
+	kotlinOptions {
+		freeCompilerArgs = listOf("-Xjsr305=strict")
+		jvmTarget = "11"
+	}
+}
+
+tasks.test {
+	finalizedBy(tasks.jacocoTestReport)
+}
+
+tasks.jacocoTestReport {
+	dependsOn(tasks.test)
+	finalizedBy(tasks.jacocoTestCoverageVerification)
+}
+
+tasks.jacocoTestCoverageVerification {
+	violationRules {
+		rule {
+			limit {
+				counter = "LINE"
+				value = "COVEREDRATIO"
+				minimum = "0.80".toBigDecimal()
+			}
+			limit {
+				counter = "BRANCH"
+				value = "COVEREDRATIO"
+				minimum = "0.50".toBigDecimal()
+			}
+		}
+	}
 }
 
 githook {
-    failOnMissingHooksDir = false
-    createHooksDirIfNotExist = false
-    hooks {
-        create("commit-msg") {
-            task = "commitlint -Dmsgfile=\$1"
-        }
-    }
+	failOnMissingHooksDir = false
+	createHooksDirIfNotExist = false
+	hooks {
+		create("commit-msg") {
+			task = "commitlint -Dmsgfile=\$1"
+		}
+	}
 }
