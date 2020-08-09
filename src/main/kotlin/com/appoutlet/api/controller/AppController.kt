25af43f23a6b3cee900afdb42816c935927350ad
Messias Junior <messiaslima.03@gmail.com>
2020-08-09 17:30:52 +0100

    feat: save view count

--- src/main/kotlin/com/appoutlet/api/controller/AppController.kt
+++ src/main/kotlin/com/appoutlet/api/controller/AppController.kt
@@ -2,6 +2,8 @@ package com.appoutlet.api.controller
 
 import com.appoutlet.api.service.application.AppService
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
@@ -12,4 +14,7 @@ class AppController(
 ) {
 	@GetMapping
 	fun getAllApplications() = appService.findAll()
+
+	@PostMapping("{appId}/view")
+	fun registerAppView(@PathVariable appId: String) = appService.registerVisualization(appId)
 }
