6b966af5be0996a5bdde91a37858959d9a2c59b0
Messias Junior <messiaslima.03@gmail.com>
2020-08-11 16:44:17 +0100

    test: enhance flathub synchronizer tests

--- src/main/kotlin/com/appoutlet/api/model/flathub/FlathubApplication.kt
+++ src/main/kotlin/com/appoutlet/api/model/flathub/FlathubApplication.kt
@@ -2,13 +2,5 @@ package com.appoutlet.api.model.flathub
 
 data class FlathubApplication(
     val flatpakAppId: String,
-    val name: String? = null,
-    val summary: String? = null,
-    val iconDesktopUrl: String? = null,
-    val iconMobileUrl: String? = null,
-    val currentReleaseVersion: String? = null,
-    val currentReleaseDate: String? = null,
-    val inStoreSinceDate: String? = null,
-    val rating: Int? = null,
-    val ratingVotes: Int? = null
+    val name: String? = null
 )
