ab937a95117bca1801ee359c3006bcf62ff157c7
Messias Junior <messiaslima.03@gmail.com>
2020-08-11 16:44:17 +0100

    test: enhance flathub synchronizer tests

--- src/main/kotlin/com/appoutlet/api/model/flathub/FlathubScreenshot.kt
+++ src/main/kotlin/com/appoutlet/api/model/flathub/FlathubScreenshot.kt
@@ -1,7 +1,5 @@
 package com.appoutlet.api.model.flathub
 
 data class FlathubScreenshot(
-    val thumbUrl: String? = null,
-    val imgMobileUrl: String? = null,
     val imgDesktopUrl: String
 )
--- src/test/kotlin/com/appoutlet/api/service/synchronization/FlathubSynchronizerTest.kt
+++ src/test/kotlin/com/appoutlet/api/service/synchronization/FlathubSynchronizerTest.kt
@@ -1,7 +1,5 @@
 package com.appoutlet.api.service.synchronization
 
-import com.appoutlet.api.model.ApplicationPackageType
-import com.appoutlet.api.model.ApplicationStore
 import com.appoutlet.api.model.appoutlet.AppOutletApplication
 import com.appoutlet.api.model.flathub.FlathubApplication
 import com.appoutlet.api.model.flathub.FlathubApplicationDetails
@@ -11,41 +9,36 @@ import com.appoutlet.api.repository.AppOutletApplicationRepository
 import com.appoutlet.api.repository.flathub.FlathubRepository
 import io.mockk.every
 import io.mockk.mockk
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Assertions.assertFalse
+import org.junit.jupiter.api.Assertions.assertNull
 import org.junit.jupiter.api.Assertions.assertTrue
-import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
 import reactor.core.publisher.Flux
 import reactor.core.publisher.Mono
+import reactor.kotlin.core.publisher.toMono
 import java.util.Date
 
 @DisplayName("Flathub synchronizer")
 internal class FlathubSynchronizerTest {
 
-	private val mockFlathubRepository = mockk<FlathubRepository>()
-	private val mockAppOutletApplicationRepository = mockk<AppOutletApplicationRepository>()
-	private val mockSynchronizationProperties = mockk<SynchronizationProperties>()
-	private lateinit var flathubSynchronizer: FlathubSynchronizer
+	private val flathubRepositoryMock = mockk<FlathubRepository>()
+	private val appOutletApplicationRepositoryMock = mockk<AppOutletApplicationRepository>()
+	private val synchronizationPropertiesMock = mockk<SynchronizationProperties>()
 
-	@BeforeEach
-	fun setup() {
-		every { mockSynchronizationProperties.flathub.enabled }.returns(false)
-		flathubSynchronizer = FlathubSynchronizer(
-			mockFlathubRepository,
-			mockAppOutletApplicationRepository,
-			mockSynchronizationProperties
-		)
-	}
+	private fun getFlathubSynchronizer() = FlathubSynchronizer(
+		flathubRepositoryMock,
+		appOutletApplicationRepositoryMock,
+		synchronizationPropertiesMock
+	)
 
 	@Test
 	fun `Should synchronize `() {
 		val flatpakApp1 = "flatpak.app.1"
-		val flatpakApp2 = "flatpak.app.2"
 
 		val applications = listOf(
-			FlathubApplication(flatpakAppId = flatpakApp1),
-			FlathubApplication(flatpakAppId = flatpakApp2)
+			FlathubApplication(flatpakAppId = flatpakApp1)
 		)
 
 		val applicationDetails = FlathubApplicationDetails(
@@ -64,11 +57,33 @@ internal class FlathubSynchronizerTest {
 			currentReleaseDate = Date(),
 			inStoreSinceDate = Date(),
 			categories = listOf(FlathubCategory("Category1")),
-			screenshots = listOf(FlathubScreenshot(imgDesktopUrl = "/screenshot1.png"))
+			screenshots = listOf(
+				FlathubScreenshot(imgDesktopUrl = "https://path.to/screenshot1.png")
+			)
 		)
 
-		val applicationDetails2 = FlathubApplicationDetails(
-			flatpakAppId = flatpakApp2,
+		every { synchronizationPropertiesMock.flathub.enabled }.returns(true)
+		every { flathubRepositoryMock.getApps() }.returns(Flux.fromIterable(applications))
+		every { flathubRepositoryMock.getApplicationDetails(flatpakApp1) }.returns(Mono.just(applicationDetails))
+		every { appOutletApplicationRepositoryMock.save<AppOutletApplication>(any()) }.answers {
+			val appOutletApp = it.invocation.args[0] as AppOutletApplication
+
+			assertEquals("https://dl.flathub.org/file.png", appOutletApp.icon)
+			assertEquals("https://dl.flathub.org/application.flatpakref", appOutletApp.downloadUrl)
+			assertEquals(listOf("Category1"), appOutletApp.tags)
+			assertEquals(listOf("https://path.to/screenshot1.png"), appOutletApp.screenshots)
+
+			return@answers appOutletApp.toMono()
+		}
+
+		assertTrue(getFlathubSynchronizer().synchronize().block() == true)
+	}
+
+	@Test
+	fun `Should add null on urls if it is invalid URIs`() {
+
+		val applicationDetails = FlathubApplicationDetails(
+			flatpakAppId = "flatpakApp1",
 			name = "Application name",
 			summary = "Application summary",
 			description = "Application description",
@@ -77,38 +92,41 @@ internal class FlathubSynchronizerTest {
 			homepageUrl = "https://application.com",
 			bugtrackerUrl = "https://application.com/issues",
 			donationUrl = "https://application.com/donate",
-			iconDesktopUrl = "invalid icon",
-			downloadFlatpakRefUrl = "/application.flatpakref",
+			iconDesktopUrl = "",
+			downloadFlatpakRefUrl = "invalid uri",
 			currentReleaseVersion = "1.0",
 			currentReleaseDate = Date(),
 			inStoreSinceDate = Date(),
 			categories = listOf(FlathubCategory("Category1")),
-			screenshots = listOf(FlathubScreenshot(imgDesktopUrl = "/screenshot1.png"))
+			screenshots = listOf(
+				FlathubScreenshot(imgDesktopUrl = "https://path.to/screenshot1.png")
+			)
 		)
 
-		val appOutletApplication = AppOutletApplication(
-			id = "1",
-			name = "ApplicationName",
-			summary = "Application summary",
-			description = "Application description",
-			developer = "Developer",
-			license = "MIT",
-			store = ApplicationStore.FLATHUB,
-			packageType = ApplicationPackageType.FLATPAK
+		val applications = listOf(
+			FlathubApplication(flatpakAppId = applicationDetails.flatpakAppId)
 		)
 
-		every { mockSynchronizationProperties.flathub.enabled }.returns(true)
-		every { mockFlathubRepository.getApps() }.returns(Flux.fromIterable(applications))
-		every { mockFlathubRepository.getApplicationDetails(flatpakApp1) }.returns(Mono.just(applicationDetails))
-		every { mockFlathubRepository.getApplicationDetails(flatpakApp2) }.returns(Mono.just(applicationDetails2))
-		every { mockAppOutletApplicationRepository.save<AppOutletApplication>(any()) }.returns(Mono.just(appOutletApplication))
 
-		assertTrue(flathubSynchronizer.synchronize().block() == true)
+
+		every { synchronizationPropertiesMock.flathub.enabled }.returns(true)
+		every { flathubRepositoryMock.getApps() }.returns(Flux.fromIterable(applications))
+		every { flathubRepositoryMock.getApplicationDetails(applicationDetails.flatpakAppId) }.returns(Mono.just(applicationDetails))
+		every { appOutletApplicationRepositoryMock.save<AppOutletApplication>(any()) }.answers {
+			val appOutletApp = it.invocation.args[0] as AppOutletApplication
+
+			assertNull(appOutletApp.icon)
+			assertNull(appOutletApp.downloadUrl)
+
+			return@answers appOutletApp.toMono()
+		}
+
+		assertTrue(getFlathubSynchronizer().synchronize().block() == true)
 	}
 
 	@Test
 	fun `Should not synchronize if property is false `() {
-		every { mockSynchronizationProperties.flathub.enabled }.returns(false)
-		assertFalse(flathubSynchronizer.synchronize().block() == true)
+		every { synchronizationPropertiesMock.flathub.enabled }.returns(false)
+		assertFalse(getFlathubSynchronizer().synchronize().block() == true)
 	}
 }
