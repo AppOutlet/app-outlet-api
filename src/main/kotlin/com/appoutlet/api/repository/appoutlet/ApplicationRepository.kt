751b8e580a459d0d1a0ae427cab0fe6ae1e03c39
Messias Junior <messiaslima.03@gmail.com>
2020-08-12 17:55:10 +0100

    feat: search application

--- src/main/kotlin/com/appoutlet/api/repository/appoutlet/ApplicationRepository.kt
+++ src/main/kotlin/com/appoutlet/api/repository/appoutlet/ApplicationRepository.kt
@@ -1,6 +1,11 @@
 package com.appoutlet.api.repository.appoutlet
 
 import com.appoutlet.api.model.appoutlet.AppOutletApplication
+import org.springframework.data.mongodb.repository.Query
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository
+import reactor.core.publisher.Flux
 
-interface ApplicationRepository : ReactiveMongoRepository<AppOutletApplication, String>
+interface ApplicationRepository : ReactiveMongoRepository<AppOutletApplication, String> {
+	@Query("{ \$or : [{ name: { \$regex: '?0', \$options: 'i' }}, { summary: { \$regex: '?0', \$options: 'i' }}, { description: { \$regex: '?0', \$options: 'i' }}] }")
+	fun searchNameOrDescription(searchTerm: String): Flux<AppOutletApplication>
+}
--- src/main/kotlin/com/appoutlet/api/repository/appoutlet/CustomApplicationRepository.kt
+++
@@ -1,8 +0,0 @@
-package com.appoutlet.api.repository.appoutlet
-
-import com.appoutlet.api.model.appoutlet.AppOutletApplication
-import reactor.core.publisher.Flux
-
-interface CustomApplicationRepository {
-	fun searchNameOrDescription(searchTerm: String): Flux<AppOutletApplication>
-}
